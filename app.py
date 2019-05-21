53dc1af1845f86c5df023e597711a4fc131632b2
philip-owen <owen.philipb@gmail.com>
2019-05-21 16:12:03 -0500

    moved image tag from jquery to jinja template to be passed to jquery ajax get function

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ import requests
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def base():
     return render_template('base.html')
 
 @app.route('/getGoodBoi')
 def goodBoi():
     url = 'https://dog.ceo/api/breeds/image/random'
     response = requests.get(url).json()
-    return jsonify(response)
+    return render_template('image.html', url=response['message'])
 
 if __name__ == "__main__":
     app.run()
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Good Bois
+
+A simple flask app utilizing the [dog api](http://www.dog.ceo/dog-api/).
