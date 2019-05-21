9d79a1b5226a568884528e930370ec827fc1c4b0
philip-owen <owen.philipb@gmail.com>
2019-05-21 13:33:41 -0500

    added /getGoodBoi route to return dog image url

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+@app.route('/getGoodBoi')
+def goodBoi():
+    url = 'https://dog.ceo/api/breeds/image/random'
+    response = requests.get(url).json()
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run()
